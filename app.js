ce0b2b9a6cf74638a479f91c110020c0c3225f37
alex simi <alex_simi_jr@yahoo.com>
2016-08-23 22:31:40 +0300

    moved to angular material

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = angular.module('app',['ngMaterial']);
+
+app.controller('search', function($scope){
+    $scope.simpleSearch = function(){
+        window.location = 'https://www.google.ro/#q=' + $scope.simpleSearchModel;
+    }
+
+    $scope.filetypeSearch = function(){
+        window.location = 'https://www.google.ro/#q=filetype:' + $scope.filetypeSearchTypeModel + ' ' + $scope.filetypeSearchNameModel;
+    }
+
+    $scope.siteSearch = function(){
+        window.location = 'https://www.google.ro/#q=site:' + $scope.siteSearchUrlModel + ' ' + $scope.siteSearchTextModel;
+    }
+
+    $scope.weatherSearch = function(){
+        window.location = window.location = 'https://www.google.ro/#q=weather ' + $scope.weatherSearchPlaceModel + ' ' + $scope.weatherSearchDateModel;
+    }
+
+    $scope.definitionSearch = function(){
+        window.location = 'https://www.google.ro/#q=define ' + $scope.definitionSearchModel;
+    }
+
+    $scope.sinanSearch = function(){
+        window.location = 'https://www.google.ro/#q='+ $scope.sinanSearchSinanModel + ' ' + $scope.sinanSearchWordModel;
+    }
+
+    $scope.titleSearch = function(){
+        window.location = 'https://www.google.ro/#q=intitle:' + $scope.titleSearchModel;
+    }
+
+    $scope.relatedSearch = function(){
+        window.location = 'https://www.google.ro/#q=related:' + $scope.relatedSearchModel;
+    }
+});
